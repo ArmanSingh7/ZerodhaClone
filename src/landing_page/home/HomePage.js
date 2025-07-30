4c21b19b471a08372e069a777dde7a47366253fa
ArmanSingh7 <businessapp7781@gmail.com>
2025-07-30 18:13:48 +0530

    Project initiate + some additional

---
+++ src/landing_page/home/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Awards from './Awards';
+import Hero from './Hero';
+
+function HomePage() {
+    return ( 
+        <>
+            <Hero/>
+            <Awards/>
+            
+        </>
+     );
+}
+
+export default HomePage;
